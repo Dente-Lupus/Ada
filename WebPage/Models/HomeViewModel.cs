71b8fe5e55b319e817562a16275d7b49bb603ca2
Exequiel Beker <exequielbeker@gmail.com>
2021-01-22 16:01:07 -0300

    Implemented Partial Views

---
+++ WebPage/Models/HomeViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ada.Webpage.Models
+{
+    public class HomeViewModel
+    {
+        public List<BannerItem> Banner { get; set; }
+        public List<PostSummary> Posts { get; set; }
+    }
+
+    public class BannerItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Uri Image { get; set; }
+        public Uri Action { get; set; }
+    }
+
+    public class PostSummary
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Uri Header { get; set; }
+        public Uri PostUri { get; set; }
+    }
+}
